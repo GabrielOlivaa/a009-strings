758af34bb15333d38158f0ca6f3e5bb4c3c64010
GabrielOlivaa <gedfto@gmail.com>
2022-08-31 15:12:39 -0300

    exercicio 03ok

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -3,8 +3,7 @@
 const frase =  ` Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
 console.log(frase)
 
-const frase1= ` Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair "`
-const Nfrase1= frase.replace("verde", "rosa").replace("azul", "laranja")
-console.log(Nfrase1)
+const Nfrase= frase.replace("verde", "rosa").replace("azul", "laranja")
+console.log(Nfrase)
 
-console.log(Nfrase1. replaceAll("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR"))
+console.log(Nfrase. replaceAll("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR"))
