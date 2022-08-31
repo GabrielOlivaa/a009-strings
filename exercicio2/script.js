89327eefef26cf63101d68903fb6138a775886d8
GabrielOlivaa <gedfto@gmail.com>
2022-08-31 14:50:37 -0300

    exercicio 02ok

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,18 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      "
+console.log(minhaString)
+console.log("A quantidade de caracters com espaço é:",minhaString.length) 
+
+//---------------------------------------------------------------------------------------------------
+
+const minhaString2= (minhaString.length)
+console.log(minhaString.trim())
+console.log("Quantidade de caracters sem espaço:",minhaString.trim().length)
+
+
+
+
+
+//---------------------------------------------------------------------------------------------------
+const MinhaString= "Eu não sou supersticioso, mas sou um pouco ________.      "
+const trcMinhaString= MinhaString.replaceAll(" ________","sticioso")
+console.log(trcMinhaString)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
 //Crie a const para a frase aqui
+
+const frase =  ` Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+console.log(frase)
+
+const frase1= ` Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair "`
+const Nfrase1= frase.replace("verde", "rosa").replace("azul", "laranja")
+console.log(Nfrase1)
+
+console.log(Nfrase1. replaceAll("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR"))
