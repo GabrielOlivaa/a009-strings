7563f75385562ab6c1b35c85f22390df8fd2feca
GabrielOlivaa <gedfto@gmail.com>
2022-08-31 15:07:27 -0300

    exercicio 02 ok

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -4,15 +4,14 @@ console.log("A quantidade de caracters com espaço é:",minhaString.length)
 
 //---------------------------------------------------------------------------------------------------
 
-const minhaString2= (minhaString.length)
-console.log(minhaString.trim())
-console.log("Quantidade de caracters sem espaço:",minhaString.trim().length)
+
+console.log(minhaString)
+console.log("Quantidade de caracters sem espaço é:",minhaString.trim().length)
 
 
 
 
 
 //---------------------------------------------------------------------------------------------------
-const MinhaString= "Eu não sou supersticioso, mas sou um pouco ________.      "
-const trcMinhaString= MinhaString.replaceAll(" ________","sticioso")
-console.log(trcMinhaString)
+const MinhaString= "Eu não sou supersticioso, mas sou um pouco ________.      "   
+console.log(MinhaString.replaceAll(" ________","sticioso"))
